df1df1ae4008bec3e043291de1da9612deb3780a
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-27 22:50:42 +0300

    Add retrofit dependencies

---
+++ app/src/main/java/com/uchumi/shopify/Constants.java
@@ -0,0 +1,7 @@
+package com.uchumi.shopify;
+
+
+public class Constants {
+    public static String BASE_URL = "https://rapidapi.com/3b-data-3b-data-default/api/google-shopping-data/";
+    public static String API_KEY = BuildConfig.API_KEY;
+}
