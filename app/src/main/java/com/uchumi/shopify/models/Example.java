ad8b6958b338ad9a755669ae69082760375b1797
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-29 00:17:26 +0300

    Add model classes

---
+++ app/src/main/java/com/uchumi/shopify/models/Example.java
@@ -0,0 +1,34 @@
+
+package com.uchumi.shopify;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Example {
+
+    @SerializedName("error")
+    @Expose
+    private Boolean error;
+    @SerializedName("offers")
+    @Expose
+    private Offers offers;
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public Offers getOffers() {
+        return offers;
+    }
+
+    public void setOffers(Offers offers) {
+        this.offers = offers;
+    }
+
+}
---
+++ app/src/main/java/com/uchumi/shopify/models/Offers.java
@@ -0,0 +1,35 @@
+
+package com.uchumi.shopify;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Offers {
+
+    @SerializedName("offers_count")
+    @Expose
+    private Long offersCount;
+    @SerializedName("offers")
+    @Expose
+    private List<Offer> offers = null;
+
+    public Long getOffersCount() {
+        return offersCount;
+    }
+
+    public void setOffersCount(Long offersCount) {
+        this.offersCount = offersCount;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(List<Offer> offers) {
+        this.offers = offers;
+    }
+
+}
