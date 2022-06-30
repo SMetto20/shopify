fe818cb3b52dd041ff7e7d08f4635ea00dbf8a60
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-30 14:13:36 +0300

    Create offers constructor

--- app/src/main/java/com/uchumi/shopify/ProductsAdapter.java
+++ app/src/main/java/com/uchumi/shopify/ProductsAdapter.java
@@ -1,5 +1,6 @@
 package com.uchumi.shopify;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -7,7 +8,18 @@ import android.widget.BaseAdapter;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHolder> {
+
+    Context context;
+    List<Offer> offerList;
+
+    public ProductsAdapter(Context context, List<Offer> offerList) {
+        this.context = context;
+        this.offerList = offerList;
+    }
+
     @NonNull
     @Override
     public ProductsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
