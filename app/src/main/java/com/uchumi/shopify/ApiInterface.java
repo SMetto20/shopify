5ce821ffcb01d27a57024a4cd1b7378d8f6cd24c
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-30 12:08:14 +0300

    Craete a call request

--- app/src/main/java/com/uchumi/shopify/ApiInterface.java
+++ app/src/main/java/com/uchumi/shopify/ApiInterface.java
@@ -1,6 +1,15 @@
 package com.uchumi.shopify;
 
-public interface ApiInterface {
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
+public interface ApiInterface {
+@GET("search-products")
+    Call<Offers>getOffers(
+            @Query("term") String term,
+            @Query("country") String country
+
+);
 
 }
