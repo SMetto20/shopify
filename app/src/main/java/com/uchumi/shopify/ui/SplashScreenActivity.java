bc692d272ecac397c04f362cb996035cb3cb920d
DruSadeMumba <drusademumba@gmail.com>
2022-06-30 20:56:16 +0300

    Set up SplashScreenActivity.java

--- app/src/main/java/com/uchumi/shopify/ui/SplashScreenActivity.java
+++ app/src/main/java/com/uchumi/shopify/ui/SplashScreenActivity.java
@@ -1,16 +1,39 @@
 package com.uchumi.shopify.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import com.skyfishjy.library.RippleBackground;
 import com.uchumi.shopify.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    float v = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        rippleBackground.startRippleAnimation();
+
+        mImageView = findViewById(R.id.imageView1);
+        mImageView.setTranslationX(300);
+        mImageView.setAlpha(v);
+        mImageView.animate().translationX(0).alpha(1).setDuration(2000).setStartDelay(600).start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            }
+        }, 4500);
     }
 }
